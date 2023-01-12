872bc1add41ae512f311ead22da0ddf3a3de9fec
Emre Boztaş <105316939+EmreBoztas@users.noreply.github.com>
2023-01-13 01:27:13 +0300

    Track rotation added

---
+++ Helix Jump Replica/Assets/Scripts/Movement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float rotateSpeed = 45;
+    [SerializeField] private float left;
+    [SerializeField] private float right;
+    private Vector3 currentAngle;
+
+    void Start()
+    {
+         currentAngle = transform.eulerAngles;
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Left"))
+        {
+            rotate(1);
+        }
+        if(Input.GetButtonDown("Right"))
+        {
+            rotate(-1);
+        }
+    }
+    private void rotate(int move)
+    {
+        currentAngle = new Vector3(0, currentAngle.y + (45 * move), 0);
+        transform.eulerAngles = currentAngle;
+    }
+}
